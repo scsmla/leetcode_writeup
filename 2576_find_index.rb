119b584316b3b1b99c210f6bce2dd0214d5741bd
Bui Nhat Duy <duy.buinhat@employmenthero.com>
2023-06-04 16:37:56 +0700

    Add 2576

---
+++ 2576_find_index.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Integer}
+# Link: https://leetcode.com/problems/find-the-maximum-number-of-marked-indices/description/
+def max_num_of_marked_indices(nums)
+  n = nums.length
+  nums = nums.sort
+  count = 0
+  for i in (n-n/2)..n-1
+      count += 1 if 2*nums[count] <= nums[i]
+  end
+  count*2
+end
