15e8e478785c3c0cc38936c8f9decc2824c4864e
Bui Nhat Duy <duy.buinhat@employmenthero.com>
2023-06-04 20:19:50 +0700

    Add 2568

---
+++ 2568_min_or.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} nums
+# @return {Integer}
+# Link: https://leetcode.com/problems/minimum-impossible-or/submissions/963695448/
+def min_impossible_or(nums)
+  a = Hash.new
+  for i in 0..nums.length
+      a[nums[i]] = 1
+  end
+  i = 1
+  while(!a[i].nil?)
+      i <<= 1
+  end
+  i
+end
